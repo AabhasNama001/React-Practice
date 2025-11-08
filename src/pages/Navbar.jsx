3be7ed3f913b534c6b88482b1718c50724856f21
Aabhas Nama <aabhasa9@gmail.com>
2025-11-08 12:36:28 +0530

    added navbar

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between px-10 py-5 bg-gray-900 text-2xl fixed z-10 w-full">
+    <div className="flex justify-between px-10 py-5 cursor-pointer bg-gray-900 text-2xl fixed z-10 w-full">
       <h4>Aabhas Nama</h4>
-      <div className="flex gap-x-20">
-        <h4>Home</h4>
-        <h4>Work</h4>
-        <h4>Contact</h4>
-        <h4>About</h4>
+      <div className="flex gap-x-20 transition-colors ease-in-out">
+        <h4 className="hover:text-blue-400">Home</h4>
+        <h4 className="hover:text-blue-400">Work</h4>
+        <h4 className="hover:text-blue-400">Contact</h4>
+        <h4 className="hover:text-blue-400">About</h4>
       </div>
     </div>
   );
